5a75a6b8d408f860d0b3bae0f69b889096d860b2
Theodore Roley <tvroley@uw.edu>
2023-11-05 21:43:37 -0800

    CardPlayer class and dealer and player objects

--- blackjack.js
+++ blackjack.js
@@ -1,15 +1,25 @@
-// const blackjackDeck = getDeck();
+const blackjackDeck = getDeck();
 
 // /**
 //  * Represents a card player (including dealer).
 //  * @constructor
 //  * @param {string} name - The name of the player
 //  */
-// class CardPlayer {}; //TODO
+class CardPlayer {
+    constructor(name) {
+        this.name = name;
+        this.hand = [];
+    }
+
+    drawCard() {
+        const cardIndex = Math.ceil(Math.random() * 52);
+        this.hand.push(blackjackDeck[cardIndex]);
+    }
+};
 
 // // CREATE TWO NEW CardPlayers
-// const dealer; // TODO
-// const player; // TODO
+const dealer = new CardPlayer('Dealer');
+const player = new CardPlayer('Player');
 
 // /**
 //  * Calculates the score of a Blackjack hand
